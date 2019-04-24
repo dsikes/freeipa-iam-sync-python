2d9a7e5776a28828ad15c2babd137d2226e138f3
Dan Sikes <dansikes7@gmail.com>
2019-04-24 18:50:19 -0400

    fixed nested dict issue

--- utils/freeipa.py
+++ utils/freeipa.py
@@ -44,7 +44,8 @@ def get_ipa_users():
     users = []
     client = get_ipa_client()
     for user in client.user_find(''):
-        users.append(user['uid'])
+        if 'results' in user:
+            users.append(user['results']['uid'])
     return users
 
 def add_user(username, first_name, last_name, fullname, email, ssh_keys):
