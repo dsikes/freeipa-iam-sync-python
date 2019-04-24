29a33a24c6a3aa4ab5986a27ac42cd2587ab64c7
Dan Sikes <dansikes7@gmail.com>
2019-04-24 17:15:18 -0400

    updated freeipa util to use config

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .iam import *
 from .freeipa import *
+from .config import *
--- utils/freeipa.py
+++ utils/freeipa.py
@@ -1,23 +1,25 @@
-import os
 from python_freeipa import Client
+from .config import get_config
 
-if 'IPA_HOST' not in os.environ:
+config = get_config()
+
+if 'IPA_HOST' not in config:
     raise Exception("UNABLE TO COMMUNICATE WITH FreeIPA HOST. Please set IPA_HOST env var.")
 
-if 'IPA_USERNAME' not in os.environ:
+if 'IPA_USERNAME' not in config:
     raise Exception("UNABLE TO LOGIN. Please set IPA_USERNAME env var.")
 
-if 'IPA_PASSWORD' not in os.environ:
+if 'IPA_PASSWORD' not in config:
     raise Exception("UNABLE TO LOGIN. Please set IPA_PASSWORD env var.")
 
-if 'IPA_GROUP' not in os.environ:
+if 'IPA_GROUP' not in config:
     raise Exception("UNABLE TO MANAGE GROUP. Please set IPA_GROUP env var.")
 
 
-ipa_server          = os.environ['IPA_HOST']
-ipa_group           = os.environ['IPA_GROUP']
-ipa_service_user    = os.environ['IPA_USERNAME']
-ipa_service_pass    = os.environ['IPA_PASSWORD']
+ipa_server          = config['IPA_HOST']
+ipa_group           = config['IPA_GROUP']
+ipa_service_user    = config['IPA_USERNAME']
+ipa_service_pass    = config['IPA_PASSWORD']
 
 def get_ipa_client():
     client = Client(ipa_server, version='2.230', verify_ssl=False)
