579b5e6d12e47ecb348d7d304f46c362295da1c9
Dan Sikes <dansikes7@gmail.com>
2019-04-24 14:33:50 -0400

    testing list user

--- utils/freeipa.py
+++ utils/freeipa.py
@@ -4,10 +4,21 @@ from python_freeipa import Client
 if 'IPA_HOST' not in os.environ:
     raise Exception("UNABLE TO COMMUNICATE WITH FreeIPA HOST. Please set IPA_HOST env var.")
 
+if 'IPA_USERNAME' not in os.environ:
+    raise Exception("UNABLE TO LOGIN. Please set IPA_USERNAME env var.")
+
+if 'IPA_PASSWORD' not in os.environ:
+    raise Exception("UNABLE TO LOGIN. Please set IPA_PASSWORD env var.")
+
 ipa_server = os.environ['IPA_HOST']
+ipa_service_user = os.environ['IPA_USERNAME']
+ipa_service_pass = os.environ['IPA_PASSWORD']
 
 def get_ipa_client():
-    return Client(ipa_server, version='4.6.4')
+    client = Client(ipa_server, version='4.6.4')
+    client.login(ipa_service_user, ipa_service_pass)
+    return client
 
 def get_ipa_users():
-    pass
+    client = get_ipa_client()
+    return client.group_show('ipausers')
